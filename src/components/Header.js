a93546d73cb6097fbf7acabbc37a0e6bfbca8c85
lokeshkushwah <lokesh.kushwah@gmail.com>
2025-07-18 14:47:55 +0530

    menu updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,113 +4,115 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Header() {
-  // Optional: Re-initialize AOS on mount (safe)
   useEffect(() => {
     if (typeof window !== "undefined" && window.AOS) {
       window.AOS.refresh();
     }
+
+    // Close offcanvas on link click
+    const links = document.querySelectorAll(".offcanvas .nav-link");
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        const offcanvasEl = document.querySelector(".offcanvas");
+        if (offcanvasEl) {
+          const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
+          if (bsOffcanvas) {
+            bsOffcanvas.hide();
+          }
+        }
+      });
+    });
   }, []);
 
   return (
-    <nav className="navbar  navbar-dark">
+    <nav className="navbar navbar-dark bg-transparent py-3">
       <div className="container">
-        <Link className="navbar-brand" data-aos="fade-right" href="#">
+        <Link className="navbar-brand" href="/">
           <img
             src="https://www.imsolutions.co/img/weblogo.png"
             alt="IM Logo"
             style={{ width: "80px" }}
           />
         </Link>
+
+        {/* Toggle Button */}
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
         >
-          <span className="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon"></span>
         </button>
+
+        {/* Offcanvas Menu */}
         <div
-          className="collapse navbar-collapse justify-content-center"
-          id="navbarNav"
+          className="offcanvas offcanvas-end text-bg-dark"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
         >
-          <ul className="navbar-nav " style={{ maxHeight: "500px" }}>
-            {/* Home Link */}
-            <li
-              className="nav-item px-5"
-              data-aos="fade-left"
-              data-aos-duration="1000"
-            >
-              <Link className="nav-link fw-bold fs-6 py-md-0 py-1" href="/">
-                Home
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              <Link className="navbar-brand" href="/">
+                <img
+                  src="https://www.imsolutions.co/img/weblogo.png"
+                  alt="IM Logo"
+                  style={{ width: "80px" }}
+                />
               </Link>
-            </li>
-            <li className="nav-item px-5">
-              <Link
-                className="nav-link fw-bold fs-6 py-md-0 py-1"
-                href="/"
-                data-aos="fade-left"
-                data-aos-duration="1200"
-              >
-                About Us
-              </Link>
-            </li>
-
-            {/* Service Dropdown */}
-            <li
-              className="nav-item dropdown px-5"
-              data-aos="fade-left"
-              data-aos-duration="1400"
-            >
-              <Link
-                className="nav-link dropdown-toggle fw-bold fs-6 py-md-0 py-1"
-                href="/"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Services
-              </Link>
-              <ul className="dropdown-menu bg-black bg-opacity-75 py-3 mt-3  ps-2" aria-labelledby="navbarDropdown">
-                <li>
-                  <Link className="dropdown-item text-white" href="/service/service-1">Service 1</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item text-white" href="/service/service-2">Service 2</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item text-white" href="/service/service-3">Service 3</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item text-white" href="#/service/service-4">Service 4</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item text-white" href="#/service/service-5">Service 5</Link>
-                </li>
-              </ul>
-            </li>
-
-            <li className="nav-item px-5">
-              <Link
-                className="nav-link fw-bold fs-6  py-md-0 py-1"
-                href="/"
-                data-aos="fade-left"
-                data-aos-duration="1600"
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li className="nav-item px-5">
-              <Link
-                className="nav-link fw-bold fs-6  py-md-0 py-1"
-                href="/"
-                data-aos="fade-left"
-                data-aos-duration="1800"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+            </h5>
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link className="nav-link fw-bold mb-3" href="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-bold mb-3" href="/about">About Us</Link>
+              </li>
+              <li className="nav-item dropdown ">
+                <Link
+                  className="nav-link dropdown-toggle fw-bold  mb-3"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                >
+                  Services
+                </Link>
+                <ul className="dropdown-menu dropdown-menu-dark ps-3">
+                  <li>
+                    <Link className="dropdown-item" href="/service/service-1">Service 1</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/service/service-2">Service 2</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/service/service-3">Service 3</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/service/service-4">Service 4</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/service/service-5">Service 5</Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-bold mb-3" href="/portfolio">Portfolio</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-bold mb-3" href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
